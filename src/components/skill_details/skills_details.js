54e9658c1ef9f612250a5c12e62f9ea2992a689d
Teddy Walter <teddywalter2016@gmail.com>
2022-01-11 11:35:24 +0200

    first bloc skills details

--- src/components/skill_details/skills_details.js
+++ src/components/skill_details/skills_details.js
@@ -1,13 +1,27 @@
 import React from "react";
 import classe from "./skills.module.css";
-
 import { Header } from "..";
+import icons from "../../constants/icons";
 
 const skillDetails = () => {
   return (
-    <div className={classe.container_details_skills}>
+    <div className={classe.container_details_main}>
       <Header isDetails />
-      <p>DETAILS</p>
+      <div className={classe.container_details}>
+        <div>
+          <span>Proficient :</span>
+          <div>
+            <img src={icons.monitor} alt="icon_" />{" "}
+            <span>Master / Influencer</span>
+          </div>
+        </div>
+        <div>
+          <span>Recommandation :</span>
+          <div>
+            <span>7</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
